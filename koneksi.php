f8e7311348fb1b395eb60daa1c4f2e30631a2c70
Fariz Taufik Syahbana <145887257+Frz3108@users.noreply.github.com>
2024-02-27 07:24:02 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'berita.c1uc0kogqape.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'adminsaga');
+define('DB_DATABASE', 'berita');
+?>
